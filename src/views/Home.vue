223c78bc560c8bc86993994638e05b64d9dc19a4
x3zvawq <x3zvawq@gmail.com>
2025-07-16 16:34:46 +0800

    feat(home): 首页内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,33 @@
 <template>
-    Home
+    <n-flex class="p-home" :align="'center'" justify="center" vertical>
+        <n-h3>一时间不知道写点啥，以后再说吧（广告位招租）</n-h3>
+        <n-text>剑三真好玩 魔盒真好用</n-text>
+        <n-text class="mb-4">支持魔盒谢谢喵~</n-text>
+        <n-flex :align="'center'" class="m-info-item">
+            <i-mdi:user class="u-icon" />
+            <n-text>作者：秀秀不咕@梦江南</n-text>
+        </n-flex>
+        <n-flex :align="'center'" class="m-info-item">
+            <img class="u-icon" src="@/assets/jx3box.svg" alt="魔盒" />
+            <n-text>
+                发布地址：<n-button text type="primary" @click="openUrl('https://www.jx3box.com/tool/101669')">
+                    JX3BOX
+                </n-button>
+            </n-text>
+        </n-flex>
+        <n-flex :align="'center'" class="m-info-item">
+            <i-mingcute:qq-fill class="u-icon" />
+            <n-text>联系方式：3303928580</n-text>
+        </n-flex>
+        <n-flex :align="'center'" class="m-info-item">
+            <i-mdi:github class="u-icon" />
+            <n-text>
+                仓库地址：<n-button text type="primary" @click="openUrl('https://github.com/X3ZvaWQ/monster-helper')">
+                    MonsterHelper
+                </n-button>
+            </n-text>
+        </n-flex>
+    </n-flex>
 </template>
 
 <script setup lang="ts">
@@ -53,4 +81,16 @@ onMounted(() => {
 </script>
 
 <style lang="less" scoped>
+.p-home {
+    height: 100%;
+
+    .m-info-item {
+        min-width: 240px;
+    }
+    .u-icon {
+        color: white;
+        width: 24px;
+        height: 24px;
+    }
+}
 </style>
